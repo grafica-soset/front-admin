bbc27375aceeebb46c1279e0d0b67a8e31d436c2
Roberto Alves <roberto@rhfactor.com.br>
2026-04-10 16:32:06 -0300

    Criar o sistema de logoff

--- app/stores/sessionStore.js
+++ app/stores/sessionStore.js
@@ -1,20 +1,24 @@
+
+const initalState = {
+    username: "",
+    nome: "",
+    token: "",
+}
+
 export const sessionStore = defineStore('session', {
-    state: () => ({
-        username: "",
-        nome: "",
-        token: "",
-    }),
+    state: () => (initalState),
     getters: {
         isLogged: (state) => state.token != null && state.token.length > 5,
     },
     actions: {
         login({ username, token }) {
-
-            console.log("LOGIN", username, token)
-
             this.username= username;
             this.token = token;
         },
+        async logout() {
+            this.username= "";
+            this.token = "";
+        }
     },
     persist: true,
 })
