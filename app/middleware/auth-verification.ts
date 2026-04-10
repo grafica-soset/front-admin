41e07fbeab664d6d73c8e68b9d250cc6a828373f
Roberto Alves <roberto@rhfactor.com.br>
2026-04-10 15:36:07 -0300

    Adicionar referencia da pagina anterior

--- app/middleware/auth-verification.ts
+++ app/middleware/auth-verification.ts
@@ -1,8 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-
-    console.log('Middleware',to, from);
-
+    // console.log('Middleware',to.query?.redirectTo , from.query?.redirectTo);
     if (to.path !== '/login') {
-        return navigateTo('/login')
+        return navigateTo('/login?redirectTo=' + to.path);
     }
 })
