e55666e900f9eef1f786331a839bd773e33637cd
Roberto Alves <roberto@rhfactor.com.br>
2026-04-19 22:22:24 -0300

    Adicionar listagem

---
+++ app/pages/listagem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const breadcrumbItems = [
+  { label: 'Exemplos', to: '/' },
+  { label: 'Listagem' } // Sem "to", renderiza como texto atual
+]
+
+
+useHead({
+  title: 'Tela de exemplo de listagem'
+})
+</script>
+
+<template>
+  <div class="max-w-5xl mx-auto">
+
+    <UiBreadcrumb :items="breadcrumbItems" />
+
+    <UiPageHeader title="Lista de clientes">
+    </UiPageHeader>
+
+
+
+  </div>
+</template>
+
+<style scoped>
+/* Estilos extra apenas para este componente podem vir aqui, mas usamos as globais no main.css */
+</style>
