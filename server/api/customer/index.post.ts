7189f3354b0e3a4a0b75a8d1804106a7c5c860a4
DaviMarcell <88511068+zFronTyyDZ7@users.noreply.github.com>
2026-04-21 15:07:37 -0300

    Update index.post.ts
    envia cadastro para o backend (testado com servidor)

--- server/api/customer/index.post.ts
+++ server/api/customer/index.post.ts
@@ -7,37 +7,27 @@ export default defineEventHandler(async (event) => {
     const { apiServer } = config
     const { token, ...customerData } = body
 
-    // remover este bloco quando o backend estiver rodando
-    console.log("dados recebidos", customerData)
-   await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise(resolve => setTimeout(resolve, 2000));
 
-    return {
-        success: true,
-        message: 'Cliente criado com sucesso',
-        data: customerData
+    try {
+        const response = await $fetch(`${apiServer}/api/customer`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            body: customerData
+        })
+        return {
+            success: true,
+            message: 'Cliente criado com sucesso',
+            data: response
+        }
+    } catch (e) {
+        console.log("Create Customer Error", e)
+        return {
+            success: false,
+            message: 'Erro ao criar cliente',
+        }
     }
-
-    //
-
-    // try {
-    //     const response = await $fetch(`${apiServer}/api/customer`, {
-    //         method: 'POST',
-    //         headers: {
-    //             Authorization: `Bearer ${token}`
-    //         },
-    //         body: customerData
-    //     })
-    //     return {
-    //         success: true,
-    //         message: 'Cliente criado com sucesso',
-    //         data: response
-    //     }
-    // } catch (e) {
-    //     console.log("Create Customer Error", e)
-    //     return {
-    //         success: false,
-    //         message: 'Erro ao criar cliente',
-    //     }
-    // }
 
 })
